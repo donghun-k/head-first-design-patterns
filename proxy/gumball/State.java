3e46319c85700eeb9adc6afc77fb3ef690b417c2
DongHun, Kim <donghun.kdh@gmail.com>
2022-12-03 23:31:37 +0900

    Proxy Pattern - gumball

---
+++ proxy/gumball/State.java
@@ -0,0 +1,16 @@
+package proxy.gumball;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+
+  public void insertQuarter();
+
+  public void ejectQuarter();
+
+  public void turnCrank();
+
+  public void dispense();
+
+  public void refill();
+}
